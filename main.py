946b6cd5a80218f141ffdb5069a77e9f61d3e176
Prasad Ostwal <prasadostwal@gmail.com>
2019-01-18 00:37:55 +0530

    Added more functions in my_funcs.py
    Removed clutter from main.py

--- main.py
+++ main.py
@@ -16,119 +16,51 @@ from sklearn.datasets import load_digits
 from sklearn.metrics import mean_absolute_error
 from scipy import stats
 from scipy.stats import pearsonr,spearmanr
-import my_funcs
+import my_funcs as func
+
 
 # Load boston into Pandas Dataframe
 data=load_boston()
 boston = pd.DataFrame(data.data, columns=data.feature_names) 
 boston['Price'] = data.target
 
-#Appreciation DIS 
-if True:
-    print(boston['DIS'].describe())
-    #plt.hist(np.abs(stats.zscore(boston['DIS'])),100)
-    plt.hist(boston['DIS'],200)
-    plt.show()
-    sns.boxplot(x=boston['DIS'])
-    plt.show() 
-
-#removing outliers and plotting 'em
-if True:
-    sd = np.std(boston['DIS'], axis=0)
-    print(sd)
-    DISprocessed =[x for x in boston['DIS'] if(x > np.mean(boston['DIS'],axis=0) - 2 * sd)]
-    DISprocessed =[x for x in DISprocessed if(x < np.mean(boston['DIS'],axis=0) + 2 * sd)]
-    sns.boxplot(x=DISprocessed)
-    plt.show()
-    plt.hist(DISprocessed,200)
-    plt.show()
-
-#Normmalizing and standardizing Boston data 
-if True:
-    normalizedBoston = preprocessing.normalize(boston)
-    standardizedBoston = preprocessing.scale(boston)
-    normPlusStandBoston = preprocessing.scale(normalizedBoston)
-
-
-    
 # Train and fit data 
 if True:
     y = boston['Price']
     x = boston.drop('Price',axis=1)
-    #sklearn.model_selection.train_test_split
+    
+    #Split Train and Test set
     xtrain, xtest, ytrain,ytest= sklearn.model_selection.train_test_split(x,y,test_size=0.2)
+    
+    #Linear Regression
     lm= LinearRegression()
     lm.fit(xtrain,ytrain)
     ypred = lm.predict(xtest) 
     ypred2 = lm.predict(xtrain)
-
- 
- 
-# xtrain = Training set of x    
-# xtest = Testing set of x
-# ytrain = Training set of y  
-# ytest = Testing set of y
-# ypred = Predict set of y   
-# ypred2 = Predict      
     
-#Plot predictions ypred vis-a-vis ytest
-t= np.arange(ypred.size)
-plt.plot(t,ypred)
-plt.plot(t,ytest)
-plt.show()
-plt.scatter(ypred,ytest) 
-plt.show()
-#plt.figure(figsize=(20,16))
-plt.plot(t,ytest*0,'k')
-plt.scatter(t,ytest-ypred) 
-plt.show()
-
-#Plot predictions ypred2 vis-a-vis ytrain
-t= np.arange(ypred2.size)
-plt.plot(t,ypred2)
-plt.plot(t,ytrain)
-plt.show()
-plt.scatter(ypred2,ytrain) 
-plt.show()
-plt.plot(t,ytrain*0,'k')
-plt.scatter(t,ytrain-ypred2) 
-plt.show()
-
-#Covariance , pearson and spearman
-if True:
-    cov = np.cov(boston['TAX'],boston['INDUS'])
-    pearson, _ = pearsonr(boston['TAX'],boston['INDUS'])
-    corr, _ = spearmanr(boston['TAX'],boston['INDUS'])
-    plt.scatter(boston['TAX'],boston['INDUS'])
-    plt.show()
-    print(cov)
-    print("Pearson")
-    print(pearson)
-    print("Spearman")
-    print(corr)
+    #Ridge
+    rr = Ridge(alpha=0.1)
+    rr.fit(xtrain, ytrain)
+    ypredRidge = rr.predict(xtest)
     
-    cov = np.cov(boston['NOX'],boston['DIS'])   
-    pearson, _ = pearsonr(boston['NOX'],boston['DIS'])
-    corr, _ = spearmanr(boston['NOX'],boston['DIS'])
-    plt.scatter(boston['NOX'],boston['DIS'])
-    plt.show()
-    print(cov)
-    print("Pearson")
-    print(pearson)
-    print("Spearman")
-    print(corr)    
-    plt.figure(figsize=(15,8))
+    #Lasso
+    lasso= Lasso()
+    lasso.fit(xtrain,ytrain)
+    ypredLasso = lasso.predict(xtest)
+    
+    #PLotting Functions 
+    func.my_scatter(ytest,ypred,"ypred-ytest",xname="ytest",yname="ypred",size=5,color='g')
+    func.my_scatter(ytrain,ypred2,"ypred2-ytrain",xname="ytrain",yname="ypred2",size=5,color='b')
+    func.my_scatter(ytest,ypredRidge,"ytest- YpredRidge",xname="ytest",yname="ypredRidge",size=5,color='y')
+    func.my_scatter(ytest,ypredLasso,"ytest- YpredLasso",xname="ytest",yname="ypredLasso",size=5,color='m')
+    
+    #Printing Errors
+    func.my_errors(ytest,ypred,title="Ytest -  Ypred")
+    func.my_errors(ytrain,ypred2,title="Ytrain - Ypred")
+    func.my_errors(ytest,ypredRidge,title="Ytrain - YpredRidge")
+    func.my_errors(ytest,ypredLasso,title="Ytrain - YpredLasso")
     
     
-    sns.heatmap(boston.corr(), annot=True)  
-        
-#Check Error values
-if True:
-    mse = sklearn.metrics.mean_squared_error(ytest, ypred)
-    mae = sklearn.metrics.mean_absolute_error(ytest, ypred)
-    print("MSE: %f, MAE: %f"%(mse,mae))
 
-
-
-
-
+    
+    
--- my_funcs.py
+++ my_funcs.py
@@ -2,6 +2,7 @@
 #Auth: Prasad Ostwal
 #Changelog
 #   v1.0 -17Jan - Initial commit: Made functions from orignal temp.py
+# Subsequent functions will be added to this file 
 
 #Importing modules 
 import pandas as pd
@@ -13,21 +14,22 @@ from datetime import date,datetime
 import sklearn as sklearn
 from sklearn import preprocessing
 from sklearn.datasets import load_boston
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression,Ridge,Lasso
 import sklearn.metrics
 from sklearn.datasets import load_digits
-from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
 from scipy import stats
 from scipy.stats import pearsonr,spearmanr
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
 
 #Function to calculate different error values 
-def calc_errors(d_orig,d_pred,print_select):
-    mse = sklearn.metrics.mean_squared_error(d_orig, d_pred)
-    mae = sklearn.metrics.mean_absolute_error(d_orig, d_pred)
-    rmse = np.sqrt(sklearn.metrics.mean_squared_error(d_orig, d_pred))
-    r2 = sklearn.metrics.r2_score(d_orig,d_pred)
-    if print_select is True:
-        print("Error Values:\n\t1.MSE:%d\n\t2.MAE:%d\n\t3.RMSE:%d\n\t4.R2:%d\n"%(mse,mae,rmse,r2))
+def my_errors(d_orig,d_pred,title=None):
+    mse = round(sklearn.metrics.mean_squared_error(d_orig, d_pred),2) 
+    rmse = round(np.sqrt(sklearn.metrics.mean_squared_error(d_orig, d_pred)),2)
+    mae = round(sklearn.metrics.mean_absolute_error(d_orig, d_pred),2)
+    r2 = round(sklearn.metrics.r2_score(d_orig,d_pred),2)
+    print("Error Values for %s:\n\t1.MSE: %s\n\t2.MAE: %s\n\t3.RMSE: %s\n\t4.R2: %s\n"%(title,mse,mae,rmse,r2))
     return mse,mae,rmse,r2
 
 #Scatter function, so just calling one liner with args will plot the graph instead of multiple lines
@@ -41,12 +43,12 @@ def my_scatter(x,y,title,xname,yname,size,color,**kwargs):
     plt.show()
 
 # Pairgrid with time function. Will also return time required to plot 
-def my_pairgrid(data,save):
+def my_pairgrid(data,title,save=None):
     now = time.time()
     my_plot = sns.PairGrid(data)
-    my_plot = my_plot.map_upper(sns.scatter,s=20)
+    my_plot = my_plot.map_upper(plt.scatter,s=20)
     my_plot = my_plot.map_lower(sns.kdeplot, cmap="Blues_d")
-    my_plot = my_plot.map_diag(sns.hist)     
+    my_plot = my_plot.map_diag(plt.hist)     
     xlabels,ylabels = [],[]
     for ax in my_plot.axes[-1,:]:
         xlabel = ax.xaxis.get_label_text()
@@ -60,6 +62,24 @@ def my_pairgrid(data,save):
             my_plot.axes[j,i].yaxis.set_label_text(ylabels[j])    
     if save is True:
         my_plot.savefig("hunny %s"%(datetime.now().date().today()))
-    exec_time = time.time() - now
-    return exec_time
+        print("Fig saved in root")
+    return "Time required to plot graph is " + str(int(time.time()-now)) + " seconds."
     
+#remove outliers from 'data' and plot boxplot if plot=True, 
+def my_remove_outliers(data,plot,sdvalue):
+         sd = np.std(data, axis=0)
+         processed =[x for x in data if(x > np.mean(data,axis=0) - sdvalue * sd)]
+         processed =[x for x in processed if(x < np.mean(data,axis=0) + sdvalue * sd)]
+         if plot is True:
+             sns.boxplot(data) #before
+             plt.title("Before Processing")
+             plt.show()
+             sns.boxplot(processed) #after
+             plt.title("After Processing")
+             plt.show()
+
+# This function is used for fetching variable name as string  during plotting axis names             
+def get_name(variable):
+ for name in globals():
+     if eval(name) is variable:
+         return name
